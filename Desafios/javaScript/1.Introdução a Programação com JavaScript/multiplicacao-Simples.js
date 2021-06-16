2ab9a98befdda82fa415038baaa119c9dfa7f354
rodrigoSilva23 <rodriguinho2456@gmail.com>
2021-06-16 10:40:45 -0300

    Desafios

--- Desafios/javaScript/1.Introdução a Programação com JavaScript/multiplicacao-Simples.js
+++ Desafios/javaScript/1.Introdução a Programação com JavaScript/multiplicacao-Simples.js
@@ -3,7 +3,7 @@ Desafio
 Você receberá dois valores inteiros. Faça a leitura e em seguida calcule o produto entre estes dois valores. Atribua esta operação à variável PROD, mostrando esta de acordo com a mensagem de saída esperada (exemplo abaixo).   
 
 Entrada
-A entrada contém 2 valores inteiros.
+A entrada contém 2 valores inteiros. 
 
 Saída
 Exiba a variável PROD conforme exemplo abaixo, tendo obrigatoriamente um espaço em branco antes e depois da igualdade.
@@ -12,5 +12,5 @@ Exiba a variável PROD conforme exemplo abaixo, tendo obrigatoriamente um espaç
 
 let valor1 = parseInt(gets());
 let valor2 = parseInt(gets());
-let total =valor1 * valor2; // Altere o valor da variável com o cálculo esperado 
+let total =valor1 * valor2; // Altere o valor da variável com o cálculo esperado  
 console.log("PROD = " + total);
